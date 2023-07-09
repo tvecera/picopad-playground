fb07a017c2b5502476851492707c7fed421a6884
Michal Schwarz <michal.schwarz@gmail.com>
2023-07-09 21:23:30 +0200

    rom.c with include

--- picopad-sdk/picopad-gb/src/rom.c
+++ picopad-sdk/picopad-gb/src/rom.c
@@ -1,4 +1,6 @@
 // xxd -i rom.gb rom.c
 
 #include <pico/platform.h>
-const unsigned char __in_flash("rom") gameRom[] = {};
+const unsigned char __in_flash("rom") gameRom[] = {
+    #include "../../../_make/rom_array_content_only.c"
+};
