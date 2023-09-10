5dbf4c489bac003ada529e762b8a8ccac15c369d
KevinarCZ <j.kevinar@gmail.com>
2023-09-10 15:40:01 +0200

    Update repository

--- picopad-sdk/applications/gl5516_orig/build/picopad_base/src/common/hcsr04/CMakeFiles/hcsr04_headers_hcsr04_pio_h.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hcsr04_headers_hcsr04_pio_h"
-  "hcsr04.pio.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hcsr04_headers_hcsr04_pio_h.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
