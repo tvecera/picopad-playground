a4be729a716a9fa75afdb09940fc405ffe128d2f
KevinarCZ <j.kevinar@gmail.com>
2023-09-17 13:33:34 +0200

    Edited code description

--- picopad-sdk/applications/pir/src/main.cpp
+++ picopad-sdk/applications/pir/src/main.cpp
@@ -1,48 +1,51 @@
-// Includes necessary libraries
+//Includes necessary libraries
 #include "pico/printf.h"
 
-// Graphics and display libraries
+//Graphics and display libraries
 #include "st7789.h"
 #include "lib_drawtft.h"
 
-// Picopad specific libraries
+//Picopad specific libraries
 #include "picopad_init.h"
 #include "picopad_key.h"
 #include "picopad_fonts.h"
 
-// Utility libraries
+//Utility libraries
 #include "sdk_watchdog.h"
 
 int main() {
 
-    // Initialize device
+    //Initialize device
     DeviceInit(true);
-    
-    // Nastavení pinu PIR senzoru
-    const uint pir_pin = 28; // Změňte podle zapojení
+        
+    //Set PIN and GPIO
+    const uint pirPin = 28;
+    gpio_init(pirPin);
+    gpio_set_dir(pirPin, GPIO_IN);
 
-    gpio_init(pir_pin);
-    gpio_set_dir(pir_pin, GPIO_IN);
-
+    //Variable for PIR sensor
     bool motionDetected = false;
     
+     //Text array for PIR sensor
     char motionText[30];
     char motionDetectText[30];
 
     do {
-        // Clear the display
+        //Clear the display
         DrawClear(COL_BLACK);
+        
+        //PIR sensor status
+        bool motion = gpio_get(pirPin);
 
-        bool motion = gpio_get(pir_pin);
-
+        //Motion is detected
         if (motion && !motionDetected) {
             motionDetected = true;
-            
             DrawClear(COL_BLACK);
             snprintf(motionDetectText, sizeof(motionDetectText), "ALARM! Motion detected!");
             DrawText2(motionDetectText, 105, 120, COL_RED);
             DispUpdate();
         } 
+        //The end of motion detected
         else if (!motion && motionDetected) {
             motionDetected = false;
             DrawClear(COL_BLACK);
@@ -51,9 +54,8 @@ int main() {
             DispUpdate();
         }
 
-        // Continue this loop until a key press is detected
+    //Continue this loop until a key press is detected
     } while (KeyGet() == NOKEY);
-
-    // Reset to bootloader when a key is pressed
+    //Reset to bootloader when a key is pressed
     ResetToBootLoader();
 }
